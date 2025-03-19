a9bfe1f4ad8ab83a041cc68db202e87049f9b966
Your Name <you@example.com>
2025-03-18 19:07:03 -0700

    awsproject

--- backend.sh
+++ backend.sh
@@ -40,7 +40,7 @@ npm install &>>$LOGFILE
 VALIDATE $? "installing nodesjs dependencies"
 
 #check your repo and path
-cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+cp /home/ec2-user/Shell-Practice/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 VALIDATE $? "Copying backend service"
 
 systemctl daemon-reload &>>$LOGFILE
